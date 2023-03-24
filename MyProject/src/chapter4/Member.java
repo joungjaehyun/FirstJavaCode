05b3b3e27d1b118b0d483d460b4b18cb7a512def
joungjaehyun <joungjaehyun1008@gmail.com>
2023-03-24 17:46:13 +0900

    Update Member.java

--- MyProject/src/chapter4/Member.java
+++ MyProject/src/chapter4/Member.java
@@ -1,46 +1,45 @@
 package chapter4;
-import java.time.LocalDate;
+
+import java.util.Scanner;
+
 public class Member {
 
-
-	 
 	public static void main(String[] args) {
-
-		int age = checkAge(1990);
+		Scanner in = new Scanner(System.in);
+		System.out.println("검진 대상자 체크");
+		System.out.println("태어난 년도를 입력하세요. >>");
 		
-		if (age<15 || age>=65) {
-			System.out.println("무료예방접종이 가능합니다.");
-		}
-		else 
-		{
-			System.out.println("무료접종 대상이 아닙니다.");
-		}
-
-	
-	
-		if (age >= 20) {
-			System.out.println("무료로 2년마다 건강 검진 가능");
-			if (age >= 40) {
-				System.out.println("40세 이상은 암검사도 무료로 가능");
-			}
-		}
-		if (age % 2 == 0) {
-			System.out.println("올해 무료 건강검진 대상입니다.");
-		} else {
-			System.out.println("올해 무료 건강검진 대상이 아닙니다.");
-		}
+		int year = in.nextInt();
+		
+		printCheckFreeVaccine(year);
+		printCheckMedical(year);
 	}
+
+static void printCheckFreeVaccine (int year) {
 	
+	int age = 2023 - year;
 	
-	
-	//태어난 해를 매개변수로 받고, 그값을 int타입으로 리턴하는 checkAge함수 정의
-	
-	static int checkAge (int birthYear) {
-		LocalDate now = LocalDate.now();
-		int year = now.getYear();
-		
-		return year-birthYear;
-		
+	if (age < 15 || age >= 65) {
+		System.out.println("무료예방접종이 가능합니다.");
+	}else {
+		System.out.println("무료접종 대상이 아닙니다.");
 	}
 	
 }
+
+static void printCheckMedical (int year) {
+	
+	int age = 2023 - year;
+	
+	if (age>=20 &&(2023+year)% 2== 0) {
+		System.out.println("무료 검진 대상입니다.");
+		if (age >= 40) {
+			System.out.println("무료 암 검진 대상자 입니다.");
+		}
+	}else {
+		System.out.println("무료 검진 대상이 아닙니다.");
+	}
+	}
+}
+	
+
